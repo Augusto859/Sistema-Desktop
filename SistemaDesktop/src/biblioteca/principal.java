630cf87105bb80ad509c1102925fa0e41f369061
Augusto859 <usersalles823@gmail.com>
2026-07-07 15:46:08 -0300

    atualizei o main

--- SistemaDesktop/src/biblioteca/principal.java
+++ SistemaDesktop/src/biblioteca/principal.java
@@ -14,25 +14,46 @@ public class principal {
 		Scanner input_texto = new Scanner(System.in);
 		System.out.println("Selecione uma das opções acima: ");
 		String opcao = input_texto.nextLine();
-
+		
+		
+		
+		
 		if (opcao.equals("1")) {
-			cadastrar_livro cadastro = new cadastrar_livro();
-			cadastro.nome_autor();
-			cadastro.nome_livro();
-			cadastro.data_publicacao();
-			cadastro.sinopse();
-			cadastro.preco();
-
-			System.out.println("\n--- LIVRO CADASTRADO COM SUCESSO ---");
-			System.out.printf("Título: %s%n", cadastro.nome_autor);
-			System.out.printf("Autor: %s%n", cadastro.nome_livro);
-			System.out.printf("Ano: %s%n", cadastro.data_publicacao);
-			System.out.printf("Sinopse: %s%n", cadastro.sinopse);
-			System.out.printf("Preço: R$%.2f%n", cadastro.preco);
-			System.out.println("------------------------------------");
+			String continuar_cadastro;
 			
-			listaLivros.add(cadastro);
-
+			do {
+				cadastrar_livro cadastro = new cadastrar_livro();
+				cadastro.nome_autor();
+				cadastro.nome_livro();
+				cadastro.data_publicacao();
+				cadastro.sinopse();
+				cadastro.preco();
+	
+				System.out.println("\n--- LIVRO CADASTRADO COM SUCESSO ---");
+				System.out.printf("Título: %s%n", cadastro.nome_autor);
+				System.out.printf("Autor: %s%n", cadastro.nome_livro);
+				System.out.printf("Ano: %s%n", cadastro.data_publicacao);
+				System.out.printf("Sinopse: %s%n", cadastro.sinopse);
+				System.out.printf("Preço: R$%.2f%n", cadastro.preco);
+				System.out.println("------------------------------------");
+				
+				listaLivros.add(cadastro);
+				
+				System.out.println("\n[1] Cadastrar outro livro");
+		        System.out.println("[2] Voltar para o menu principal");
+		        System.out.print("Escolha uma opção: ");
+		        continuar_cadastro = input_texto.nextLine();
+		        
+		        while (!continuar_cadastro.equals("1") && !continuar_cadastro.equals("2")) {
+		            System.out.println("\nDigite um número das opções acima!\n");
+		            
+		            System.out.println("[1] Cadastrar outro livro");
+		            System.out.println("[2] Voltar para o menu principal");
+		            System.out.print("Escolha uma opção válida: ");
+		            continuar_cadastro = input_texto.nextLine(); // Pede para digitar novamente
+		        }
+		        
+			} while (continuar_cadastro.equals("1"));
 		}
 
 		else if (opcao.equals("2")) {
